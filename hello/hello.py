f5d92c48b8332a0b4f065403929dd370bf830a24
Christian Wander <cwshit@posteo.de>
2023-06-12 16:02:55 +0200

    extend example

--- hello/hello.py
+++ hello/hello.py
@@ -1,20 +1,28 @@
 import requests
 
+
 def say_hello():
     print('hello from my_function')
 
 
-def another_function():
-    print('hello from another_function')
+def hello_function() -> str:
+    """ Returns the string `hello` """
+    return "hello"
 
 
 def cat_fact() -> str:
+
+    hello_function()
+
     r = requests.get('https://cat-fact.herokuapp.com/facts/random')
     
     result = r.json()
-    print(result['text'])
+    fact = result['text']
+    print(fact)
+
+    return fact
 
 
 if __name__ == "__main__":
     """Say Hello"""
-    say_hello()
+    cat_fact()
